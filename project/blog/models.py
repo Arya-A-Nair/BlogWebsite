668fe4039a0d17eccb89120dfc129b519f7050a2
Killjoy <aryaajitnair@gmail.com>
2023-02-01 00:51:23 +0530

    feat: Initialised the setup of app

---
+++ project/blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
+# Create your models here.
+
+class Category(models.Model):
+    name=models.TextField(unique=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural="categories"
+
+
+class Blog(models.Model):
+    title=models.TextField()
+    authorName=models.ForeignKey(User,related_name="created_by",on_delete=models.DO_NOTHING)
+    description=models.TextField()
+    category=models.ManyToManyField(Category,related_name='categories')
+    content=models.TextField()
+
+    def __str__(self):
+        return self.title
+    
